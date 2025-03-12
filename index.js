1dae468cae4da3c4db2d6e461e2cae9d7ed90243
muzakl <themuzawz@gmail.com>
2025-03-12 14:11:52 +0400

    Front changes

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+}
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', async (req, res) => {
-    res.json({status : "Server is running!"})
-
+app.get('/menus/navbar', async (req, res) => {
     async function run() {
         try {
             const database = await connectToDatabase();
-            // const users = database.collection('users');
-            // const cursor = users.find();
-            // const result = await cursor.toArray()
-            // res.json(result);
+            const users = database.collection('menus');
+            const cursor = users.find();
+            const result = await cursor.toArray()
+            res.json(result);
         } catch (error) {
             console.error('Error handling request:', error);
             res.status(500).json({error: 'Internal server error'});
