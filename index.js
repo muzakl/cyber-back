c7f86afef4b4b77bca51f4aaba96b37862a393a4
muzakl <themuzawz@gmail.com>
2025-03-23 15:52:44 +0400

    Final commit before revamping for final project

--- index.js
+++ index.js
@@ -30,6 +30,23 @@ app.get('/menus/navbarItems', async (req, res) => {
     run().catch(console.dir);
 });
 
+app.get('/menus/categoryItems', async (req, res) => {
+    async function run() {
+        try {
+            const database = await connectToDatabase();
+            const categoryItems = database.collection('categoryItems');
+            const cursor = categoryItems.find();
+            const result = await cursor.toArray()
+            res.json(result);
+        } catch (error) {
+            console.error('Error handling request:', error);
+            res.status(500).json({error: 'Internal server error'});
+        }
+    }
+
+    run().catch(console.dir);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- services/database.js
+++ services/database.js
@@ -1,6 +1,6 @@
 import {MongoClient} from "mongodb";
 
-const uri = "mongodb+srv://muzakl:1234@cluster0.mnnz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://test:test@cluster0.zijsk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 async function connectToDatabase() {
